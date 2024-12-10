5352f9fa36aa71a682351f3da2dab430f7ef2528
NoahJaspers <noah.jaspers@student.ucll.be>
2024-12-10 13:43:31 +0100

    gemaakt

---
+++ script.js
@@ -0,0 +1,25 @@
+// const changeTitle = document.getElementById("changetitle");
+// const title = document.getElementById("title");
+// changeTitle.addEventListener("click", click);
+// function click() {
+//   title.textContent = "javaScript is geweldig";
+// }
+// const paragraph = document.getElementById("paragraph");
+// const addText = document.getElementById("addText");
+// const tekst = document.createElement("paragraph");
+// addText.addEventListener("click", () => {
+//   paragraph.innerHTML += "<p>Hier is extra tekst!</p>";
+// });
+
+// const colorBox = document.getElementById("colorBox");
+// const changeColor = document.getElementById("changeColor");
+// changeColor.addEventListener("click", () => {
+//   const rood = Math.round(Math.random() * 255);
+//   const groen = Math.round(Math.random() * 255);
+//   const blauw = Math.round(Math.random() * 255);
+//   colorBox.style.backgroundColor = `rgb(${rood},${groen},${blauw})`;
+// });
+
+const button = document.getElementById("hideText");
+const text = document.getElementById("text");
+button.addEventListener("click", () => {});
